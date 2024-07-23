8c0a953022beabf551e7a2a1de2731e5d56add7c
lorde78 <basil.lombard@hetic.net>
2024-07-23 12:28:17 +0200

    Fix : j'ai retiré le composants csgostats qui ne servait a rien et aussi la liste des stats récupérés depuis l'api steam

--- frontend/src/components/Profil.tsx
+++ frontend/src/components/Profil.tsx
@@ -2,52 +2,44 @@ import React from "react";
 import GameCard from "./GameCard";
 
 interface ProfilProps {
-    pseudo: string;
-    jeux: { nom: string; heures: number; image: string; appid: number }[];
-    photoProfil: string;
-    csgoStats: { name: string; value: number }[];
+	pseudo: string;
+	jeux: { nom: string; heures: number; image: string; appid: number }[];
+	photoProfil: string;
+	csgoStats: { name: string; value: number }[];
 }
 
 const Profil: React.FC<ProfilProps> = ({
-    pseudo,
-    jeux,
-    photoProfil,
-    csgoStats,
+	pseudo,
+	jeux,
+	photoProfil,
+	csgoStats,
 }) => {
-    const csgoGame = jeux.find(jeu => jeu.appid === 730);
+	const csgoGame = jeux.find((jeu) => jeu.appid === 730);
 
-    return (
-        <div className="profile">
-            <img
-                className="profile-item"
-                src={photoProfil}
-                alt={`${pseudo}'s profile`}
-            />
-            <h1 className="profile-item">{pseudo}</h1>
-            <h2 className="profile-item">Liste des jeux</h2>
-            {csgoGame ? (
-                <div className="game-list profile-item">
-                    <GameCard 
-                        nom={csgoGame.nom} 
-                        image={csgoGame.image} 
-                        heures={csgoGame.heures} 
-                    />
-                </div>
-            ) : (
-                <p className="profile-item">Désolé, vous n'avez pas de jeux compatible.</p>
-            )}
-            <h2 className="profile-item">CS:GO Stats</h2>
-            <div className="profile-item stats-container">
-                <ul>
-                    {csgoStats.map((stat, index) => (
-                        <li key={index}>
-                            {stat.name}: {stat.value}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+	return (
+		<div className="profile">
+			<img
+				className="profile-item"
+				src={photoProfil}
+				alt={`${pseudo}'s profile`}
+			/>
+			<h1 className="profile-item">{pseudo}</h1>
+			<h2 className="profile-item">Liste des jeux</h2>
+			{csgoGame ? (
+				<div className="game-list profile-item">
+					<GameCard
+						nom={csgoGame.nom}
+						image={csgoGame.image}
+						heures={csgoGame.heures}
+					/>
+				</div>
+			) : (
+				<p className="profile-item">
+					Désolé, vous n'avez pas de jeux compatible.
+				</p>
+			)}
+		</div>
+	);
 };
 
 export default Profil;
